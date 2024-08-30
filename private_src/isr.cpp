4480ee1dd109e48af8416f4d1ebf1e0a159371f1
hjc2000 <hjc.666@foxmail.com>
2024-08-30 17:20:06 +0800

    try     {         func();     }     catch (...)     {     }

--- private_src/isr.cpp
+++ private_src/isr.cpp
@@ -1,58 +1,89 @@
 #include <bsp-interface/di/interrupt.h>
 #include <hal.h>
 
-static void SafeRun(std::function<void()> &func)
-{
-    try
-    {
-        func();
-    }
-    catch (...)
-    {
-    }
-}
-
 extern "C"
 {
     void TIM3_IRQHandler()
     {
         std::function<void()> &func = DI_IsrManager().GetIsr(static_cast<uint32_t>(IRQn_Type::TIM3_IRQn));
-        SafeRun(func);
+        try
+        {
+            func();
+        }
+        catch (...)
+        {
+        }
     }
 
     void WWDG1_IRQHandler()
     {
         std::function<void()> &func = DI_IsrManager().GetIsr(static_cast<uint32_t>(IRQn_Type::WWDG_IRQn));
-        SafeRun(func);
+        try
+        {
+            func();
+        }
+        catch (...)
+        {
+        }
     }
 
     void EXTI0_IRQHandler()
     {
         std::function<void()> &func = DI_IsrManager().GetIsr(static_cast<uint32_t>(IRQn_Type::EXTI0_IRQn));
-        SafeRun(func);
+        try
+        {
+            func();
+        }
+        catch (...)
+        {
+        }
     }
 
     void EXTI1_IRQHandler()
     {
         std::function<void()> &func = DI_IsrManager().GetIsr(static_cast<uint32_t>(IRQn_Type::EXTI1_IRQn));
-        SafeRun(func);
+        try
+        {
+            func();
+        }
+        catch (...)
+        {
+        }
     }
 
     void EXTI2_IRQHandler()
     {
         std::function<void()> &func = DI_IsrManager().GetIsr(static_cast<uint32_t>(IRQn_Type::EXTI2_IRQn));
-        SafeRun(func);
+        try
+        {
+            func();
+        }
+        catch (...)
+        {
+        }
     }
 
     void EXTI3_IRQHandler()
     {
         std::function<void()> &func = DI_IsrManager().GetIsr(static_cast<uint32_t>(IRQn_Type::EXTI3_IRQn));
-        SafeRun(func);
+        try
+        {
+            func();
+        }
+        catch (...)
+        {
+        }
     }
 
     void EXTI4_IRQHandler()
     {
         std::function<void()> &func = DI_IsrManager().GetIsr(static_cast<uint32_t>(IRQn_Type::EXTI4_IRQn));
-        SafeRun(func);
+        try
+        {
+            func();
+        }
+        catch (...)
+        {
+        }
     }
 }
